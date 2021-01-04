3646cda46901610664c8038c436b1e90fdebd122
vishalshuklag <er_vishalshukla@outlook.com>
2021-01-04 23:54:32 +0530

    timer

---
+++ 29- Countdown Timer/js/app.js
@@ -0,0 +1,53 @@
+let countdown;
+const timerDisplay = document.querySelector('.display__time-left');
+const endTimeDisplay = document.querySelector('.display__end-time');
+const buttons = document.querySelectorAll('[data-time]');
+
+function timer(seconds) { 
+    // clear existing timer
+    clearInterval(countdown);
+ 
+
+    const now = Date.now();
+    const then = now + seconds * 1000;
+    displayTimeLeft(seconds);
+    displayEndTime(then);
+    countdown = setInterval( () => {
+        const secondsLeft = Math.round((then - Date.now() )/1000);
+        if (secondsLeft < 0) {
+            clearInterval(countdown);
+            return
+        }
+        displayTimeLeft(secondsLeft);
+    }, 1000)
+}
+
+function displayTimeLeft(seconds) {
+    const minutes = Math.floor(seconds / 60);
+    const remainderSeconds = seconds % 60;
+    const display = `${minutes}:${remainderSeconds < 10 ? '0' : ''}${remainderSeconds}`;
+    document.title = display;
+    timerDisplay.textContent = display;
+}
+
+function displayEndTime(timestamp) {
+    const end = new Date(timestamp);
+    const hours = end.getHours();
+    const minutes = end.getMinutes();
+    const adjustedHour = hours > 12 ? hours - 12 : hours;
+    endTimeDisplay.textContent = `Be Back At ${adjustedHour}:${minutes < 10 ? '0' : ''}${minutes}`;
+}
+
+function startTimer(e) {
+    const seconds = parseInt(this.dataset.time);
+    timer(seconds)
+}
+
+buttons.forEach( button => button.addEventListener('click', startTimer));
+
+document.customForm.addEventListener('submit', function (e) {
+    e.preventDefault();
+    const mins = this.minutes.value;
+    timer(mins * 60)
+    this.reset();
+})
